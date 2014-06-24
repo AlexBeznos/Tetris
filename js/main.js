61ee18b205b2afef789c8e61f11271745beda543
Alex Beznos <beznosa@yahoo.com>
2014-06-24 16:40:53 +0300

    centralize canvas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 var canvas = document.getElementById("main");
-canvas.style.marginLeft = ((screen.width - canvas.width)/2);
+canvas.style.marginLeft = (((screen.width - canvas.width)-100)/2);
 var ctx = canvas.getContext('2d');
 ctx.fillRect(0, 0, canvas.width, canvas.height);
