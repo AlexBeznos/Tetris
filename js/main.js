7bd2aaba2d1d666205d76da7a6a86ddef4f1d858
YanekDev <yan.summer23@gmail.com>
2014-06-27 00:18:18 +0300

    Work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-/* Test */
+var cantag = document.getElementById("main");
+var ctx = cantag.getContext("2d");
+
+cantag.width = '500';
+cantag.height ='1000';
+
+
+
+var RectByCoor = {
+  Draw :
+  function () {
+      
+        ctx.fillStyle = '#FF0000';
+        ctx.fillRect(0,this.index,500,100);
+        this.index += 100;  
+        setTimeout ( function () { RectByCoor.Clear();},950); 
+      
+  },
+    index : 0,
+  Clear  : function () {
+    ctx.clearRect(0,0,500,1000);
+  }
+};
+setInterval ( function () { RectByCoor.Draw();},1000);
+
