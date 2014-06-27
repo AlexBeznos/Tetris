951daebd37d360410caab2353efd156c1e7c2a5e
Alex Beznos <beznosa@yahoo.com>
2014-06-27 08:42:13 +0300

    Unstoped movement of different blocks and abillity to move only untill canvas will be ended

--- js/main.js
+++ js/main.js
@@ -1,35 +1,80 @@
 var cantag = document.getElementById("main");
 var ctx = cantag.getContext("2d");
 
-cantag.width = '500';
-cantag.height ='1000';
+cantag.width = '250';
+cantag.height ='500';
 
-// Function for blocks moving
-var i = 0;
+var timeInterval = 40;
+
+
+// function for saving blocks on the game field
+var savedBlocks = [];
+var blockSaver = function(hash, x, y) {
+  hash.ex = x;
+  hash.ey = y;
+  savedBlocks.push(hash);
+  var img = new Image();
+  img.src = "/sprites/json.png";
+  console.log(savedBlocks[0]);
+    ctx.drawImage(img, savedBlocks[0].x, 
+                  savedBlocks[0].y, 
+                  savedBlocks[0].w, 
+                  savedBlocks[0].h, 
+                  10, 
+                  savedBlocks[0].ey, 
+                  savedBlocks[0].w, 
+                  savedBlocks[0].h
+                 );
+  randomiser();
+};
+
+
+// Calculator of block stop
+var calculator = function(height, key) {
+  var numb = cantag.height - height;
+  var answer = (key < numb) ? true : false;
+  return answer
+};
 
 // Draw image
 var imgDrawer = function(object) {
+  var i = 0;
   var img = new Image();
   img.src = "/sprites/json.png";
   img.onload = function() {
-    setInterval( function() {
-      i++;
-      ctx.drawImage(img, object.x, object.y, object.w, object.h, 0, i, object.w, object.h);
-    }, 1000);
+    var interval = setInterval( function() {
+      i+=2;
+      ctx.clearRect ( 0, i-2, object.w , object.h );
+      var calc = calculator(object.h, i); 
+      if(calc) {
+        ctx.drawImage(img, object.x, 
+                      object.y, 
+                      object.w, 
+                      object.h, 
+                      0, i, 
+                      object.w, 
+                      object.h);
+      } else if (!calc) { clearInterval(interval); blockSaver(object, 0, i); }; //send object, x and y for saving on the game field
+    }, timeInterval);
   }
+};
+
+// Randomiser
+var randomiser = function() {
+  var rand = Math.floor((Math.random() * 7) + 0);
+  imgDrawer(spriteSheet[rand]);
 };
 
 // Image array maker
 var spriteSheet = [];
 var imageArrayMaker = function(hash) {
   var newHash = {};
-  var rand = Math.floor((Math.random() * 7) + 0);
   for(var key in hash) {
     newHash = hash[key].frame;
     newHash.name = key;
     spriteSheet.push(newHash);
   };
-  imgDrawer(spriteSheet[rand]);
+  randomiser();
 };
 
 // Parser for json
