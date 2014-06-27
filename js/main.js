25a93e8dae740ee67f596649288740663ba41bad
Alex Beznos <beznosa@yahoo.com>
2014-06-27 06:15:19 +0300

    Reafctoring

--- js/main.js
+++ js/main.js
@@ -4,37 +4,44 @@ var ctx = cantag.getContext("2d");
 cantag.width = '500';
 cantag.height ='1000';
 
-//strang img bug
-var img = new Image();
-img.src = "/sprites/json.png";
-ctx.drawImage(img, 0, 0);
-
 
 // Draw image
 var imgDrawer = function(object) {
   var img = new Image();
   img.src = "/sprites/json.png";
-  ctx.drawImage(img, object.x, object.y, object.w, object.h, 0, 0, object.w, object.h);
+  img.onload = function() {
+    ctx.drawImage(img, object.x, object.y, object.w, object.h, 0, 0, object.w, object.h);
+  }
 };
 
 // Image array maker
 var spriteSheet = [];
 var imageArrayMaker = function(hash) {
   var newHash = {};
+  var rand = Math.floor((Math.random() * 7) + 0);
   for(var key in hash) {
     newHash = hash[key].frame;
     newHash.name = key;
     spriteSheet.push(newHash);
   };
-  imgDrawer(spriteSheet[6]);
+  imgDrawer(spriteSheet[rand]);
+};
+
+// Parser for json
+var parser = function(req) {
+  var parsed = JSON.parse(req.responseText);
+  var frame = parsed.frames;
+  imageArrayMaker(frame); 
 };
 
 // Request to json
-var xhr = new XMLHttpRequest;
-xhr.open("GET", "/sprites/json", true);
-xhr.onload = function() {
-  var parsed = JSON.parse(xhr.responseText);
-  var frame = parsed.frames;
-  imageArrayMaker(frame);  
+var request = function() {
+  var xhr = new XMLHttpRequest;
+  xhr.open("GET", "/sprites/json", true);
+  xhr.onload = function() {
+    parser(xhr); 
+  };
+  xhr.send();
 };
-xhr.send();
+
+request();
